9b37560a4b8572115d1f80fb7ca2de821bf03d4b
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-10-11 08:11:54 +0300

    Добавьте файлы проекта.

---
+++ Lesson 18/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_18
+{
+    class Class1
+    {
+        public delegate string ExampleDelegate(int arg1, long arg2);
+
+        public event ExampleDelegate _exampleEventHandler;
+
+        public void CreateBinds()
+        {
+            _exampleEventHandler += (i, l) =>
+            {
+                if (i > l)
+                {
+                    return "Greater";
+                }
+                else return "Less";
+
+                _exampleEventHandler += Print;
+                _exampleEventHandler -= Print;
+            };
+        }
+
+        public void CreateBinds2()
+        {
+            var _func = new Func<int, int>((t) =>
+            {
+                return t * t; 
+            });
+
+            var r = _func.Invoke(13);
+        
+        }
+
+        public string Print(int arg1, long arg2)
+        {
+            Console.WriteLine("call");
+            return string.Empty;
+        }
+
+        public string CallBackMethod()
+        {
+            return _exampleEventHandler?.Invoke(10, 20);
+        }
+    }
+}
---
+++ Lesson 18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Class1();
+
+            Console.WriteLine(r.CallBackMethod());
+            r.CreateBinds();
+            Console.WriteLine(r.CallBackMethod());
+        }
+    }
+}
